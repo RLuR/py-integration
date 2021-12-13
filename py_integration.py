ce7956263b9afe4f21260f5eebe3c125b55f268a
raphael <raphael.lutz@gmx.at>
2021-12-13 11:20:53 +0100

    cleaned function calls and added pi test

--- py_integration.py
+++ py_integration.py
@@ -1,28 +1,32 @@
 import argparse
 import random
 import numpy as np
+import math
 
 MONTE_CARLO = 0
 RIEMANN = 1
 
-def integrate_string(functionstring, iterations, lower_boundry, upper_boundry, method):
+def integrate_string(functionstring, lower_boundry, upper_boundry, method=1, iterations=10000):
     function = _parse_Function(functionstring)
 
     if method == 0:
-        return monte_carlo_integrate(function,iterations, lower_boundry, upper_boundry)
+        return monte_carlo_integrate(function, lower_boundry, upper_boundry, iterations)
     if method == 1:
-        return riemann_integrate(function, iterations, lower_boundry, upper_boundry)
+        return riemann_integrate(function, lower_boundry, upper_boundry, iterations)
 
-def riemann_integrate(function, iterations, lower_boundry, upper_boundry):
+def riemann_integrate(function, lower_boundry, upper_boundry, iterations=10000):
     step_size = (upper_boundry - lower_boundry)/iterations
     steps = np.arange(lower_boundry, upper_boundry, step_size)
     function_array = np.vectorize(function)
     results = function_array(steps)
     return np.mean(results) * (upper_boundry - lower_boundry)
 
-def monte_carlo_integrate(function, iterations, lower_boundry, upper_boundry, min_height, max_height):
-    results = 0
+def monte_carlo_integrate(function, lower_boundry, upper_boundry, iterations, min_height = None, max_height= None):
 
+    if min_height is None or max_height is None:
+        min_height, max_height = _generate_y_boundries(function, lower_boundry, upper_boundry, iterations)
+
+    results = 0
     for step in range(1, iterations):
         random_point = _generate_random_point(lower_boundry, upper_boundry, min_height, max_height)
         stepresult = _monte_Carlostep(function, random_point)
@@ -31,10 +35,6 @@ def monte_carlo_integrate(function, iterations, lower_boundry, upper_boundry, mi
 
     minresult = (upper_boundry - lower_boundry) * (min_height)
     return minresult + (upper_boundry - lower_boundry) * (max_height - min_height) * results / iterations
-
-def monte_carlo_integrate(function, iterations, lower_boundry, upper_boundry):
-    min_height, max_height = _generate_y_boundries(function, lower_boundry, upper_boundry, iterations)
-    return monte_carlo_integrate(function, iterations, lower_boundry, upper_boundry, min_height, max_height)
 
 def _generate_random_point(lower_boundry, upper_boundry, min_height, max_height):
     return [random.uniform(lower_boundry,upper_boundry), random.uniform(min_height, max_height)]
@@ -68,12 +68,10 @@ if __name__ == '__main__':
     argumentparser.add_argument("function", type=str)
     argumentparser.add_argument("lower_boundry", type=float)
     argumentparser.add_argument("upper_boundry", type=float)
-
     args=argumentparser.parse_args()
 
-    iterations = 100000
+    iterations = 10000
 
-    print(integrate_string(args.function, iterations, args.lower_boundry, args.upper_boundry, MONTE_CARLO))
-    print(integrate_string(args.function, iterations, args.lower_boundry, args.upper_boundry, RIEMANN))
+    print(f"Monte Carlo approximation: {integrate_string(args.function, args.lower_boundry, args.upper_boundry, MONTE_CARLO, iterations)}")
+    print(f"Riemann approximation: {integrate_string(args.function, args.lower_boundry, args.upper_boundry, RIEMANN, iterations)}")
 
-    iterations = iterations * 2
--- test/TestApproximatePi.py
+++ test/TestApproximatePi.py
@@ -1,3 +1,17 @@
-import PyIntegration
-#TO-DO
+import math
+import py_integration
+import unittest
+
+class TestApproximatePi(unittest.TestCase):
+
+    def approximation_function(self, x):
+        return math.sqrt(1 - x**2)
+
+    def test_riemann_approximation(self):
+        approximate_py_half = py_integration.riemann_integrate(self.approximation_function, -1, 1)
+
+        error = abs(math.pi - approximate_py_half*2)
+
+        # less then 1 percent error
+        assert error < math.pi /100
 
