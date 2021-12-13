74a85a5b96006621a277571fec86f25751c61a68
raphael <raphael.lutz@gmx.at>
2021-12-13 14:35:48 +0100

    Added command line arguments

--- py_integration.py
+++ py_integration.py
@@ -5,6 +5,7 @@ import math
 
 MONTE_CARLO = 0
 RIEMANN = 1
+random.seed(1)
 
 def integrate_string(functionstring, lower_boundry, upper_boundry, method=1, iterations=10000):
     function = _parse_Function(functionstring)
@@ -21,7 +22,7 @@ def riemann_integrate(function, lower_boundry, upper_boundry, iterations=10000):
     results = function_array(steps)
     return np.mean(results) * (upper_boundry - lower_boundry)
 
-def monte_carlo_integrate(function, lower_boundry, upper_boundry, iterations, min_height = None, max_height= None):
+def monte_carlo_integrate(function, lower_boundry, upper_boundry, iterations=10000, min_height = None, max_height= None):
 
     if min_height is None or max_height is None:
         min_height, max_height = _generate_y_boundries(function, lower_boundry, upper_boundry, iterations)
@@ -39,14 +40,12 @@ def monte_carlo_integrate(function, lower_boundry, upper_boundry, iterations, mi
 def _generate_random_point(lower_boundry, upper_boundry, min_height, max_height):
     return [random.uniform(lower_boundry,upper_boundry), random.uniform(min_height, max_height)]
 
-
 def _parse_Function(expression):
     funcstr= '''\
 def f(x):
     return {e}'''.format(e=expression)
     exec(funcstr, globals())
     return f
-
 
 def _generate_y_boundries(function, lower_x_boundry, upper_x_boundry, iterations=1000):
     step_size = (upper_x_boundry - lower_x_boundry) / iterations
@@ -62,16 +61,20 @@ def _monte_Carlostep(function, point):
     else:
         return False
 
-
 if __name__ == '__main__':
     argumentparser = argparse.ArgumentParser()
     argumentparser.add_argument("function", type=str)
     argumentparser.add_argument("lower_boundry", type=float)
     argumentparser.add_argument("upper_boundry", type=float)
+    argumentparser.add_argument('-i', help='number of iterations (10000 default)', type=int, default=10000)
+    argumentparser.add_argument("-m", type=int, choices=[0, 1, 2], default=2,
+                        help="which approximation method used: 0 = Monte Carlo, 1 = Riemann sum, 2 = both (default)")
+
     args=argumentparser.parse_args()
 
     iterations = 10000
-
-    print(f"Monte Carlo approximation: {integrate_string(args.function, args.lower_boundry, args.upper_boundry, MONTE_CARLO, iterations)}")
-    print(f"Riemann approximation: {integrate_string(args.function, args.lower_boundry, args.upper_boundry, RIEMANN, iterations)}")
+    if args.m in [0,2]:
+        print(f"Monte Carlo approximation: {integrate_string(args.function, args.lower_boundry, args.upper_boundry, MONTE_CARLO, args.i)}")
+    if args.m in [1,2]:
+        print(f"Riemann approximation: {integrate_string(args.function, args.lower_boundry, args.upper_boundry, RIEMANN, args.i)}")
 
